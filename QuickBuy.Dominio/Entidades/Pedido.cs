e95a640466211b9b1d926174fdcbfb1a621c23b0
Emiry Mirella <emirella_jmo@hotmail.com>
2019-07-13 21:44:25 -0300

    Validações nas entidades

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,36 @@
+﻿using QuickBuy.Dominio.ObjetoDeValor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido : Entidades
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public string Cep { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public int NumeroEndereco { get; set; }
+
+        public ICollection<ItemPedido> ItensPedido { get; set; }
+        public int FormaPagamentoId { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
+
+        public override void Validate()
+        {
+            LimparMensagensValidacao();
+            if (!ItensPedido.Any())
+                AdicionarErro("Erro: Pedido não pode ficar sem Item de Pedido!");
+
+            if (string.IsNullOrEmpty(Cep))
+                AdicionarErro("Erro: o CEP precisa ser preenchido");
+
+        }
+    }
+}
